44bc628e5a4832a3a062f6ce8f814adcf415123d
ProtoPla <louwrudy007@gmail.com>
2025-05-16 04:20:47 +1200

    Help I broke the Linker Script  I have noidea what Im doing

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <cstdint>
+
+#define GPIO_ENABLE_REG   *((volatile uint32_t*) 0x3FF44020)
+#define GPIO_OUT_SET_REG  *((volatile uint32_t*) 0x3FF44008)
+#define GPIO_OUT_CLR_REG  *((volatile uint32_t*) 0x3FF4400C)
+
+#define LED_GPIO 2
+
+void delay(volatile uint32_t t) {
+    while (t--) {
+        for (volatile int i = 0; i < 10000; i++);
+    }
+}
+
+int main() {
+    GPIO_ENABLE_REG |= (1 << LED_GPIO);
+
+    while (true) {
+        GPIO_OUT_SET_REG = (1 << LED_GPIO);
+        delay(100);
+        GPIO_OUT_CLR_REG = (1 << LED_GPIO);
+        delay(100);
+    }
+}
